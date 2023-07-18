5d897f5575ba961489a0b7ce63e55eae590016b9
Arpit <guptarpit11@gmail.com>
2023-07-18 20:59:36 +0530

    To-do-app

---
+++ Todo-APP/src/main/java/com/geekster/TodoAPP/Repository/TodoRepo.java
@@ -0,0 +1,27 @@
+package com.geekster.TodoAPP.Repository;
+
+import com.geekster.TodoAPP.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TodoRepo {
+
+    @Autowired
+    private List<Todo> myTodos;
+
+    public List<Todo> getMyTodos() {
+        return myTodos;
+    }
+
+    public void add(Todo todo){
+        myTodos.add(todo);
+    }
+    public void remove(Todo todo){
+        myTodos.remove(todo);
+    }
+}
---
+++ Todo-APP/src/main/java/com/geekster/TodoAPP/controller/TodoController.java
@@ -0,0 +1,58 @@
+package com.geekster.TodoAPP.controller;
+
+import com.geekster.TodoAPP.Service.TodoService;
+import com.geekster.TodoAPP.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+
+    @Autowired
+    TodoService todoService;
+
+    public TodoController() {
+    }
+
+    @GetMapping("todos")
+    public List<Todo> getAllTodos() {
+
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("todo/done")
+    public List<Todo> getDoneTodos() {
+        return todoService.getalldonetodos();
+    }
+
+    @GetMapping("todo/undone")
+    public List<Todo> getNotDoneTodos() {
+
+        return todoService.getallundonetodos();
+    }
+
+    @PostMapping("todo")
+    public String addTodo(@RequestBody Todo todo)
+    {
+
+        return todoService.addTodos(todo);
+    }
+
+    @PutMapping("todo/{todoId}/{status}")
+    public String markTodo(@PathVariable Integer todoId,@PathVariable boolean status) {
+        return todoService.markTodos(todoId, status);
+    }
+
+    @DeleteMapping("todo")
+    public String removeTodo(@RequestParam Integer todoId)
+    {
+        return todoService.removeTodos(todoId);
+
+    }
+
+
+}
